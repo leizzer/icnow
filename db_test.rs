e79eb4a10fe5626749bb93271597a2a05b827c05
leizzer <cristian.re@datagrail.io>
2026-07-06 11:46:47 -0300

    chore: clean up stray debug and support files

--- db_test.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let home = dirs::home_dir().unwrap_or_else(|| std::path::PathBuf::from("/tmp"));
-    let mut icnow_dir = home.join(".icnow").join("db");
-    let path = "test_parse_rust.db";
-    let p = icnow_dir.join(std::path::Path::new(path).file_name().unwrap_or_else(|| std::ffi::OsStr::new("knowledge.db"))).to_string_lossy().to_string();
-    println!("{}", p);
-}
